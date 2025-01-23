51cc579197109c85676386b70708d681b0cf2cc6
n.zinovev <nervous9339@gmail.com>
2025-01-23 22:08:15 +0300

    init app

---
+++ src/main/java/com/example/anthropic/claude/AnthropicClaudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.anthropic.claude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnthropicClaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnthropicClaudeApplication.class, args);
+	}
+
+}
